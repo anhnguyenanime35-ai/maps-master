02d09fb72abbd777c4545db18b8bffa1f28bef85
anhnguyenanime35-ai <anhnguyenanime35@gmail.com>
2026-08-04 20:51:08 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+new CozeWebSDK.WebChatClient({
+    config: {
+        bot_id: "7663131576742101045"
+    },
+    auth: {
+        type: "token",
+        token: "pat_DAFGwDBH7tnVyEGpKuLkbKl3zlCIHbYmgeGsCS9lfM5Rq7tGRe0wXYVI7ko4BY8l"
+    }
+});
